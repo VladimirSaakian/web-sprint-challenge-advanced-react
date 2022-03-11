b2f325e7848c6f80fc99310da3959f32bc44e463
Verified ✔ <vladimirsaakian@gmail.com>
2022-03-11 15:38:28 -0800

    still working on it

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,6 +1,55 @@
-import React from 'react'
+import axios from 'axios'
+import React, {useState} from 'react'
 
 export default function AppFunctional(props) {
+  axios.post('http://localhost:9000/api/result', back)
+  .then(res=> {
+    setMessage(res.data.message)
+    setEmail(email)
+  })
+  .catch(err=> {
+    setMessage(err.response.data.message)
+  })
+  .finally(() => {
+    setEmail('')
+  })
+
+  const onChange = event => {
+    const {value} = event.target
+    setEmail(value)
+  }
+  const moveLeft = () => {
+    if(x > 1){
+      setX(x-1)
+      setSteps(steps + 1)
+    } else {
+      setMessage("You can't go left")
+    }
+  }
+  const moveRight = () => {
+    if(x >= 1 && x < 3){
+      setX(x+1)
+      setSteps(steps + 1)
+    } else {
+      setMessage("You can't go right")
+    }
+  }
+  const moveUp = () => {
+    if(y > 1 ) {
+      setY(y-1)
+      setSteps(steps + 1)
+    } else {
+      setMessage("You can't go up")
+    }
+  const moveDown = () => {
+    if(y >= 1 && y < 3){
+      setY(y+1)
+      setSteps(steps + 1)
+    } else {
+      setMessage("You can't go down")
+    }
+  })
+}
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
